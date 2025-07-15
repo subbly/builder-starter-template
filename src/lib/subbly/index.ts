a2639af8600f9dc83a3e65be1da6394bf7466eb2
Oleg Kobylinskyi <ilirien.oleg@gmail.com>
2025-07-15 13:19:34 +0300

    feat: re-export fetchers inside lib/subbly

--- src/lib/subbly/index.ts
+++ src/lib/subbly/index.ts
@@ -2,15 +2,23 @@ import { SubblyClient } from './subbly-client'
 import { SubblyApiClient } from '@subbly/api-client'
 
 export { SubblyScript } from './subbly-script'
+export * from './fetch/bundle-by-id'
+export * from './fetch/bundle-by-slug'
+export * from './fetch/list-bundles'
+export * from './fetch/list-products'
+export * from './fetch/load-bundle-groups'
+export * from './fetch/product-by-id'
+export * from './fetch/product-by-slug'
+export * from './fetch/variant-or-plan-by-id'
 
 if (!process.env.NEXT_PUBLIC_SUBBLY_API_KEY) {
   throw new Error('Subbly API key not found. Please add it to the .env file.')
 }
 
 export const client = SubblyApiClient.init({
-  apiKey: process.env.NEXT_PUBLIC_SUBBLY_API_KEY as string,
+  apiKey: process.env.NEXT_PUBLIC_SUBBLY_API_KEY as string
 })
 
 export const subbly = new SubblyClient({
-  client,
+  client
 })
