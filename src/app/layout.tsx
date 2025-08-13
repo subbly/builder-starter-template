ee62772ae40d7d77b15d8903b7327252941c7652
Vitalii Cherniuk <dev@abiscuit.io>
2025-08-13 21:27:44 +0800

    feat: add suppress hydration warning to body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>
           {process.env.NEXT_PUBLIC_SUBBLY_API_KEY && (
             <SubblyScript
