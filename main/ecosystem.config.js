fc7fc0c7413bc76845636fadb284d2d5f69ec863
Oleg Kobylinskyi <ilirien.oleg@gmail.com>
2025-12-26 17:03:39 +0200

    chore: ignore entire .subbly directory in pm2 watch

--- main/ecosystem.config.js
+++ main/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       interpreter: 'none',
       watch: ['src/**/*.css', 'src/**/*.scss', './pnpm-lock.yaml'],
       watch_delay: 1000,
-      ignore_watch: ['node_modules', '.git', '.next', '.subbly/logs'],
+      ignore_watch: ['node_modules', '.git', '.next', '.subbly'],
       exp_backoff_restart_delay: 100,
       log_file: './.subbly/logs/output.log',
       merge_logs: true,
